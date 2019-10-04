14f0ba311a5be390b250f6886ace889e53ad3a95
Oscar Lopez <oscarl.developer@gmail.com>
2019-10-03 17:28:18 -0700

    Added Seans_Adventure.py (unfinished)

---
+++ Seans_Adventure.py
@@ -0,0 +1,11 @@
+"""
+A simple text-based adventure of a dude named Sean.
+Author: Oscar Lopez.
+"""
+
+valid_actions = ['look', 'move', 'dance', 'drink', 'kiss', 'kick', 'punch',
+                 'eat', 'jump', 'throw', 'smoke', 'code']
+
+valid_directions = ['up', 'down', 'left', 'right', 'behind', 'forward',
+                    'backword', 'below', 'above']
+
